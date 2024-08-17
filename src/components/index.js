aee02d6b899c71908158fb36040757053223effa
ASIF <asifiqbalk99@gmail.com>
2024-08-17 19:37:55 +0530

    new components added

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,16 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+
 export { 
     Header, 
     Footer,
     Container,
     Logo,
     LogoutBtn,
+    Button,
+    Input
 
 };
